9ca2f5c7f482df899068d974225fced007eb0b93
StrokeThePea <76650942+StrokeThePea@users.noreply.github.com>
2022-06-22 12:07:32 +0200

    Update ReanimateAPI.lua

--- Libraries/ReanimateAPI.lua
+++ Libraries/ReanimateAPI.lua
@@ -1254,57 +1254,6 @@ function ReanimateAPI.PermaDeathReanimate()
 	end
 	ReanimateAPI.PlaySound(5949365181)
 end
-function ReanimateAPI.SetupFolder()
-	-- To Finish
-	if not makefolder then
-		ReanimateAPI.Notification("Missing Function (makefolder).")
-		ReanimateAPI.PlaySound(9074670249)
-		return
-	end
-	if not isfolder then
-		ReanimateAPI.Notification("Missing Function (isfolder).")
-		ReanimateAPI.PlaySound(9074670249)
-		return
-	end
-	if not writefile then
-		ReanimateAPI.Notification("Missing Function (writefile).")
-		ReanimateAPI.PlaySound(9074670249)
-		return
-	end
-	if not isfolder("FrostwareSongs") then
-		makefolder("FrostwareSongs")
-	end
-	if not isfile("FrostwareSongs/NeptunianV.mp3") then
-		writefile("FrostwareSongs/NeptunianV.mp3", game:HttpGet("https://raw.githubusercontent.com/StrokeThePea/Frostware/main/Music/Under%20Night%20In-Birth%20ost%20-%20Beat%20Eat%20Nest%20%5BExtended%5D.mp3"))
-	end
-	if not isfile("FrostwareSongs/ANIMSmoothMoves.mp3") then
-		writefile("FrostwareSongs/ANIMSmoothMoves.mp3", game:HttpGet("https://github.com/StrokeThePea/Frostware/blob/main/Music/Animations/Smooth%20Moves.mp3?raw=true"))
-	end
-	if not isfile("FrostwareSongs/ANIMOrangeJustice.mp3") then
-		writefile("FrostwareSongs/ANIMOrangeJustice.mp3", game:HttpGet("https://github.com/StrokeThePea/Frostware/blob/main/Music/Animations/Orange%20Justice.mp3?raw=true"))
-	end
-	if not isfile("FrostwareSongs/ANIMFloss.mp3") then
-		writefile("FrostwareSongs/ANIMFloss.mp3", game:HttpGet("https://github.com/StrokeThePea/Frostware/blob/main/Music/Animations/Floss.mp3?raw=true"))
-	end
-	if not isfile("FrostwareSongs/ANIMFreestylin'.mp3") then
-		writefile("FrostwareSongs/ANIMFreestylin'.mp3", game:HttpGet("https://github.com/StrokeThePea/Frostware/blob/main/Music/Animations/Freestylin'.mp3?raw=true"))
-	end
-	if not isfile("FrostwareSongs/ANIMBreakDown.mp3") then
-		writefile("FrostwareSongs/ANIMBreakDown.mp3", game:HttpGet("https://github.com/StrokeThePea/Frostware/blob/main/Music/Animations/Breakdown.mp3?raw=true"))
-	end
-	if not isfile("FrostwareSongs/ANIMElectroSwing.mp3") then
-		writefile("FrostwareSongs/ANIMElectroSwing.mp3", game:HttpGet("https://github.com/StrokeThePea/Frostware/blob/main/Music/Animations/Electro%20Swing.mp3?raw=true"))
-	end
-	if not isfile("FrostwareSongs/ANIMParrot.mp3") then
-		writefile("FrostwareSongs/ANIMParrot.mp3", game:HttpGet("https://github.com/StrokeThePea/Frostware/blob/main/Music/Animations/Parrot.mp3?raw=true"))
-	end
-	if not isfile("FrostwareSongs/ANIMCaramellDansen.mp3") then
-		writefile("FrostwareSongs/ANIMCaramellDansen.mp3", game:HttpGet("https://github.com/StrokeThePea/Frostware/blob/main/Music/Animations/Carma.mp3?raw=true"))
-	end
-	if not isfile("FrostwareSongs/ANIMBoing.mp3") then
-		writefile("FrostwareSongs/ANIMBoing.mp3", game:HttpGet("https://github.com/StrokeThePea/Frostware/blob/main/Music/Animations/Boing.mp3?raw=true"))
-	end
-end
 function ReanimateAPI.LoadMusicFromFiles(AudioInstance,Path)
 	local CustomAssetFunction = getcustomasset or getsynasset or function() end
 	if getgenv().FrostwareConfig.DisableAudio == false then
